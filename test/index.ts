b890879d704f862be9998619ac3a2e006d03e4e0
Gil Pedersen <git@gpost.dk>
2023-03-15 12:12:34 +0100

    Basic options test

--- test/index.ts
+++ test/index.ts
@@ -11,6 +11,7 @@ const { expect } = Lab.types;
 expect.type<Writable>(new UdpBlast());
 expect.type<UdpBlast>(new UdpBlast(1234));
 expect.type<UdpBlast>(new UdpBlast({ host: 'test', port: 1234 }));
+expect.type<UdpBlast>(new UdpBlast(1234, { highWaterMark: 10, ttl: 3, packetSize: 200 }));
 
 expect.error(new UdpBlast('1234'));
 
